f6ef2ae720fbdd25d1870f71412ab5518fc35b7d
Kayleigh <57508868+gordokay@users.noreply.github.com>
2022-11-11 19:52:48 -0500

    Add playRound function

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 function getComputerChoice() {
-  let options = ['rock', 'paper', 'scissors'];
+  let options = ['Rock', 'Paper', 'Scissors'];
   let choiceIndex = Math.floor(Math.random() * 3);
   return options[choiceIndex];
+}
+
+function playRound(playerSelection, computerSelection) {
+  if(playerSelection === computerSelection) {
+    return 'It\'s a draw!';
+  }
+  let round = playerSelection + computerSelection;
+  // array of player win conditions modeled as 'PlayerComputer'
+  let winConditions = ['RockScissors', 'ScissorsPaper', 'PaperRock'];
+  if(winConditions.includes(round)) {
+    return `You win! ${playerSelection} beats ${computerSelection}`;
+  }
+  return `You lose! ${computerSelection} beats ${playerSelection}`;
 }
