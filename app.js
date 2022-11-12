f1102da0d74fc021c06914962b2b5c5116cbf518
Kayleigh <57508868+gordokay@users.noreply.github.com>
2022-11-11 20:05:49 -0500

    Add game function

--- app.js
+++ app.js
@@ -16,3 +16,32 @@ function playRound(playerSelection, computerSelection) {
   }
   return `You lose! ${computerSelection} beats ${playerSelection}`;
 }
+
+function game(){
+  let playerWins = 0;
+  let computerWins = 0;
+  for(let i = 0; i < 5; i++) {
+    let playerSelection = prompt('Rock, paper, scissors, shoot!');
+    // change input to initial capital format
+    playerSelection = playerSelection.slice(0, 1).toUpperCase() + playerSelection.slice(1).toLowerCase();
+    let result = playRound(playerSelection, getComputerChoice());
+    if(result.includes('win')) {
+      playerWins++;
+    }
+    else if(result.includes('lose')) {
+      computerWins++;
+    }
+    console.log(result);
+  }
+  if(playerWins > computerWins) {
+    console.log('You win!');
+  }
+  else if(computerWins > playerWins) {
+    console.log('You lose!');
+  }
+  else {
+    console.log('It\'s a tie!');
+  }
+}
+
+game();
